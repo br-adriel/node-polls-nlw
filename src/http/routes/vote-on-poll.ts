79e6c8f9ad34a83daa4c006e8d2dcb3a70fa6b1d
Adriel <adriel.fsantos@outlook.com>
2024-02-06 14:49:53 -0300

    feat: Implement vote creation

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { randomUUID } from 'node:crypto';
 import { z } from 'zod';
+import { prisma } from '../../lib/prisma';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -30,6 +31,14 @@ export async function voteOnPoll(app: FastifyInstance) {
       });
     }
 
-    return reply.status(201).send({ sessionId });
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
+
+    return reply.status(201).send();
   });
 }
