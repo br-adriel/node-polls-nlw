7a7b699e105d4f9f13585e9fb94276a176d6e9a5
Adriel <adriel.fsantos@outlook.com>
2024-02-05 23:39:44 -0300

    feat: Add poll creation

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,24 @@
 import fastify from 'fastify';
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+app.post('/polls', async (request, reply) => {
+  const { title } = z
+    .object({
+      title: z.string(),
+    })
+    .parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return reply.status(201).send({ pollId: poll.id });
+});
 
 app.listen({ port: 3333 }).then(() => console.log('✅ HTTP SERVER RUNNING'));
