81343ec67430fe8ee185ac36f3aded23f9feab1b
Adriel <adriel.fsantos@outlook.com>
2024-02-07 19:40:40 -0300

    feat: Add option count increment on vote

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { randomUUID } from 'node:crypto';
 import { z } from 'zod';
 import { prisma } from '../../lib/prisma';
+import { redis } from '../../lib/redis';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -43,6 +44,7 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviouslyVotedOnPoll.id,
           },
         });
+        await redis.zincrby(pollId, -1, userPreviouslyVotedOnPoll.pollOptionId);
       }
     }
 
@@ -63,6 +65,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     });
+
+    await redis.zincrby(pollId, 1, pollOptionId);
 
     return reply.status(201).send();
   });
